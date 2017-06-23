fc09e97b7dbf9e7ba4d34901b35027f1fe6c3dd4
Joan Sentís <joansentis1@gmail.com>
2017-06-23 21:19:46 +0200

    added animations

--- src/app/components/postCard/postCard.component.ts
+++ src/app/components/postCard/postCard.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter, ElementRef } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 import { AppService } from '../../services/app.service';
 import { Post } from '../../models/post';
 
@@ -9,17 +10,32 @@ import { Post } from '../../models/post';
     styleUrls: ['postCard.scss'],
     host: {
         '(document:click)': 'onClickOutside($event)',
-    }
+    },
+    animations: [
+        trigger('openCommentBox', [
+            state('show', style({
+                height: '120px',
+                visibility: 'visible',
+                opacity: '1'
+            })),
+            state('hide', style({
+                height: '0',
+                visibility: 'hidden',
+                opacity: '0'
+            })),
+            transition('show <=> hide', animate('200ms ease-in-out')),
+        ]),
+    ]
 })
 
 export class postCardComponent {
     @Input() postContent: Array<any>;
     @Output() commentPublished = new EventEmitter<any>();
-    @Output() postDeleted = new EventEmitter<any>();    
+    @Output() postDeleted = new EventEmitter<any>();
     private currentPost: any;
     private commentMessage: string;
     private postSelected: any;
-    private openCommentBox: boolean = false;
+    private state: string = 'hide';
     public froalaConfig: Object = {
         placeholderText: 'Write your comment here...',
         charCounterCount: false,
@@ -27,22 +43,27 @@ export class postCardComponent {
         toolbarButtons: ['bold', 'italic', 'underline']
     };
 
-    constructor(private eRef: ElementRef, private appService: AppService) {}
+    constructor(private eRef: ElementRef, private appService: AppService) { }
+
 
     onClickOutside(event) {
         if (!this.eRef.nativeElement.contains(event.target) && !this.commentMessage)
-            this.openCommentBox = false;
+            this.state = 'hide';
     }
 
     showOpenBox(ind, post) {
         this.postSelected = ind;
-        this.openCommentBox = true;
+        this.state = (this.state === 'show' ? 'hide' : 'show');
+        if (this.eRef.nativeElement.contains(event.target)) {
+            this.state = 'show';
+            this.commentMessage = '';
+        }
     }
 
     addComment(post, comment) {
-        this.commentPublished.emit({post: post, comment: comment});
+        this.commentPublished.emit({ post: post, comment: comment });
         this.commentMessage = '';
-        this.openCommentBox = false;
+        this.state = 'hide';
     }
 
     deletePost(idReferal: string) {
