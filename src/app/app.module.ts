b8784b9c215af017dda9b9da01ff0c10d3bf5fe7
Joan Sentís <joansentis1@gmail.com>
2017-06-25 22:04:43 +0200

    Revert "Revert "added animations""
    This reverts commit a8a3bb26ac16e78d974fe43af64a26cf817df0b3.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './routes/app.routing';
 import { FroalaEditorModule, FroalaViewModule } from 'angular2-froala-wysiwyg';
@@ -16,6 +17,7 @@ import { AppService } from './services/app.service';
     BrowserModule,
     HttpModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     FroalaEditorModule.forRoot(),
     FroalaViewModule.forRoot()
   ],
