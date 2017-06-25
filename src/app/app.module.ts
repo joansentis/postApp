a8a3bb26ac16e78d974fe43af64a26cf817df0b3
Joan Sentís <joansentis1@gmail.com>
2017-06-25 22:04:23 +0200

    Revert "added animations"
    This reverts commit fc09e97b7dbf9e7ba4d34901b35027f1fe6c3dd4.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './routes/app.routing';
 import { FroalaEditorModule, FroalaViewModule } from 'angular2-froala-wysiwyg';
@@ -17,7 +16,6 @@ import { AppService } from './services/app.service';
     BrowserModule,
     HttpModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     FroalaEditorModule.forRoot(),
     FroalaViewModule.forRoot()
   ],
--- src/app/components/postCard/postCard.component.ts
+++ src/app/components/postCard/postCard.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, Output, EventEmitter, ElementRef } from '@angular/core';
-import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 import { AppService } from '../../services/app.service';
 import { Post } from '../../models/post';
 
@@ -10,32 +9,17 @@ import { Post } from '../../models/post';
     styleUrls: ['postCard.scss'],
     host: {
         '(document:click)': 'onClickOutside($event)',
-    },
-    animations: [
-        trigger('openCommentBox', [
-            state('show', style({
-                height: '120px',
-                visibility: 'visible',
-                opacity: '1'
-            })),
-            state('hide', style({
-                height: '0',
-                visibility: 'hidden',
-                opacity: '0'
-            })),
-            transition('show <=> hide', animate('200ms ease-in-out')),
-        ]),
-    ]
+    }
 })
 
 export class postCardComponent {
     @Input() postContent: Array<any>;
     @Output() commentPublished = new EventEmitter<any>();
-    @Output() postDeleted = new EventEmitter<any>();
+    @Output() postDeleted = new EventEmitter<any>();    
     private currentPost: any;
     private commentMessage: string;
     private postSelected: any;
-    private state: string = 'hide';
+    private openCommentBox: boolean = false;
     public froalaConfig: Object = {
         placeholderText: 'Write your comment here...',
         charCounterCount: false,
@@ -43,27 +27,22 @@ export class postCardComponent {
         toolbarButtons: ['bold', 'italic', 'underline']
     };
 
-    constructor(private eRef: ElementRef, private appService: AppService) { }
-
+    constructor(private eRef: ElementRef, private appService: AppService) {}
 
     onClickOutside(event) {
         if (!this.eRef.nativeElement.contains(event.target) && !this.commentMessage)
-            this.state = 'hide';
+            this.openCommentBox = false;
     }
 
     showOpenBox(ind, post) {
         this.postSelected = ind;
-        this.state = (this.state === 'show' ? 'hide' : 'show');
-        if (this.eRef.nativeElement.contains(event.target)) {
-            this.state = 'show';
-            this.commentMessage = '';
-        }
+        this.openCommentBox = true;
     }
 
     addComment(post, comment) {
-        this.commentPublished.emit({ post: post, comment: comment });
+        this.commentPublished.emit({post: post, comment: comment});
         this.commentMessage = '';
-        this.state = 'hide';
+        this.openCommentBox = false;
     }
 
     deletePost(idReferal: string) {
